5fc735d0a7c53e358a2e31cc1ed22c8b2bf9f82c
Thamires Bernardes <thamires.bernardes@gmail.com>
2021-03-25 13:56:46 -0300

    First Commit

--- src/Ex03.java
+++ src/Ex03.java
@@ -1,19 +1,35 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Ex03 {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
         double Largura, Comprimento, MetroQuadrado, Area, Preco;
+        int num1, num2, Soma;
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Entre com a largura da parede a ser pintada: ");
         Largura = sc.nextDouble();
+        System.out.println("Entre com o comprimento da parede a ser pintada:");
         Comprimento = sc.nextDouble();
+        System.out.println("Quantos mestros quadrados tem o comodo? ");
         MetroQuadrado = sc.nextDouble();
         Area = Largura * Comprimento;
         Preco = Area * MetroQuadrado;
 
-        System.out.printf("Area %.2f%n", Area);
-        System.out.printf("Preco %.2f%n", Preco);
+        System.out.printf("A Area total do projeto é %.2f%n", Area);
+        System.out.printf("E seu preco final será %.2f%n", Preco);
+
+        System.out.println("Agora entre com um número: ");
+        num1 = sc.nextInt();
+        System.out.println("Agora entre com outro número: ");
+        num2 = sc.nextInt();
+
+        Soma = num1 + num2;
+
+        System.out.println("A soma dos dois números é:" + Soma);
+
     }
 }
