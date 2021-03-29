e07d13cc1af25fe8c974f2188345c3ae9c490b63
Thamires Bernardes <thamires.bernardes@gmail.com>
2021-03-29 18:47:57 -0300

    firstCommit

--- src/Ex03.java
+++ src/Ex03.java
@@ -5,22 +5,22 @@ public class Ex03 {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
-        double Largura, Comprimento, MetroQuadrado, Area, Preco;
+        double largura, comprimento, metroQuadrado, area, preco;
         int num1, num2, num3, num4, Soma, Diferenca;
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Entre com a largura da parede a ser pintada: ");
-        Largura = sc.nextDouble();
+        largura = sc.nextDouble();
         System.out.println("Entre com o comprimento da parede a ser pintada:");
-        Comprimento = sc.nextDouble();
+        comprimento = sc.nextDouble();
         System.out.println("Quantos mestros quadrados tem o comodo? ");
-        MetroQuadrado = sc.nextDouble();
-        Area = Largura * Comprimento;
-        Preco = Area * MetroQuadrado;
+        metroQuadrado = sc.nextDouble();
+        area = largura * comprimento;
+        preco = area * metroQuadrado;
 
-        System.out.printf("A Area total do projeto é %.2f%n", Area);
-        System.out.printf("E seu preco final será %.2f%n", Preco);
+        System.out.printf("A Area total do projeto é %.2f%n", area);
+        System.out.printf("E seu preco final será %.2f%n", preco);
 
         System.out.println("Operações com números! ");
         System.out.println("Agora entre com o primeiro número: ");
@@ -32,11 +32,11 @@ public class Ex03 {
         System.out.println("Agora entre com o quarto número: ");
         num4 = sc.nextInt();
 
-        Soma = num1 + num2;
-        Diferenca = ((num1 * num2) - (num3 * num4));
+        soma = num1 + num2;
+        diferenca = ((num1 * num2) - (num3 * num4));
 
-        System.out.println("A soma dos dois primeiros números é:" + Soma);
-        System.out.println("A diferença do produto dos numeros é:" + Diferenca);
+        System.out.println("A soma dos dois primeiros números é:" + soma);
+        System.out.println("A diferença do produto dos numeros é:" + diferenca);
 
     }
 }
