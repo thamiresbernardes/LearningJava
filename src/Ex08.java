bb29eac7d9147d2720cb8446d80fab3956803122
Thamires Bernardes <thamires.bernardes@gmail.com>
2021-03-31 14:09:30 -0300

    firstCommit

---
+++ src/Ex08.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex08 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        double preco, desconto;
+
+        System.out.println("Entre com o preço do produto ");
+        preco = sc.nextDouble();
+
+        desconto = (preco < 20.0) ? preco * 0.01 : preco * 0.05;
+
+        System.out.println("O valor do desconto é: " + desconto);
+        sc.close();
+    }
+}
