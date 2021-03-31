0cddcdc89e78c6db2dfd03c881cc144884f0c936
Thamires Bernardes <thamires.bernardes@gmail.com>
2021-03-30 21:05:51 -0300

    firstCommit

--- src/Ex03.java
+++ src/Ex03.java
@@ -6,7 +6,7 @@ public class Ex03 {
 
         Locale.setDefault(Locale.US);
         double largura, comprimento, metroQuadrado, area, preco;
-        int num1, num2, num3, num4, Soma, Diferenca;
+        int num1, num2, num3, num4, soma, diferenca;
 
         Scanner sc = new Scanner(System.in);
 
@@ -22,21 +22,34 @@ public class Ex03 {
         System.out.printf("A Area total do projeto é %.2f%n", area);
         System.out.printf("E seu preco final será %.2f%n", preco);
 
-        System.out.println("Operações com números! ");
+        System.out.println("\n Operações com números!");
         System.out.println("Agora entre com o primeiro número: ");
         num1 = sc.nextInt();
         System.out.println("Agora entre com o segundo número: ");
         num2 = sc.nextInt();
         System.out.println("Agora entre com o terceiro número: ");
         num3 = sc.nextInt();
+        if (num3 % 2 == 0) {
+            System.out.println("O número digitado é par ");
+        } else {
+            System.out.println("O número digitado é ímpar ");
+        }
         System.out.println("Agora entre com o quarto número: ");
         num4 = sc.nextInt();
+        if (num4 < 0) {
+            System.out.println("O número digitado é negativo ");
+        } else {
+            System.out.println("O número digitado é positivo ");
+        }
 
         soma = num1 + num2;
         diferenca = ((num1 * num2) - (num3 * num4));
 
         System.out.println("A soma dos dois primeiros números é:" + soma);
         System.out.println("A diferença do produto dos numeros é:" + diferenca);
+        System.out.println("A diferença do produto dos numeros é:" + diferenca);
+
+        sc.close();
 
     }
 }
